86ec0a79e9ba0d9d8f9ced5c1dc7efa69012b424
Mohammed Basit Abdul <basitmohammed362@gmail.com>
2026-01-16 16:17:12 +0000

    fix: Correct SQL syntax by removing trailing newline in users table creation

--- src/main/java/org/commerce/daos/models/UsersModel.java
+++ src/main/java/org/commerce/daos/models/UsersModel.java
@@ -16,6 +16,7 @@ public class UsersModel {
                 email VARCHAR(100) UNIQUE NOT NULL,
                 password VARCHAR(255) NOT NULL,
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                
                 )
                 """;
 
